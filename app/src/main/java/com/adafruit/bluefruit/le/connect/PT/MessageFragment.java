8265b1d5b228ed04fd58a5e2289815409ebae5e9
Yuzhong Huang <yuzhong.huang@students.olin.edu>
2017-04-04 21:35:51 -0400

    add pics to messages

--- app/src/main/java/com/adafruit/bluefruit/le/connect/PT/MessageFragment.java
+++ app/src/main/java/com/adafruit/bluefruit/le/connect/PT/MessageFragment.java
@@ -45,9 +45,9 @@ public class MessageFragment extends Fragment {
         mSearch.setQueryHint("Type and search for patients");
 
         // add an example appointment
-        messages.add(new Message(new Patient("Jordan", 62, "Female", 158, 7),  "Hello, just wanna double check my current status...", false));
+        messages.add(new Message(new Patient("Jordan", 62, "Female", 158, 7),  "Hello, just wanna double check...", false));
         messages.add(new Message(new Patient("Issac", 55, "Male", 136, 6), "I will tell you my secret...", false));
-        messages.add(new Message(new Patient("Wilson", 65, "Male", 152, 5), "Hello, I have a question about my exercises...", true));
+        messages.add(new Message(new Patient("Wilson", 65, "Male", 152, 5), "Hello, I have a question about...", true));
         messages.add(new Message(new Patient("Trent", 72, "Male", 160, 6),  "Just for testing", true));
         messages.add(new Message(new Patient("Yuzhong", 61, "Male", 148, 5), "...", true));
 
